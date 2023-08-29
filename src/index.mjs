2207ecc8e83d01f452e18de423da1a89f9663870
Viet Bao <vietbao.t@gmail.com>
2023-08-29 15:36:48 +0700

    test husky

--- src/index.mjs
+++ src/index.mjs
@@ -1,8 +1,3 @@
-// const express = require('express');
-// const morgan = require('morgan');
-// const handlebars = require('express-handlebars');
-// const path = require('path');
-
 import express from 'express';
 import morgan from 'morgan';
 import { engine } from 'express-handlebars';
@@ -10,26 +5,26 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const app = express();
-const port = 3000;
+const port = 3001;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname,'public')));
+// route
+import route from './resource/routers/index.js';
+// route init
+route(app);
+//forder public
+app.use(express.static(path.join(__dirname, 'public')));
 //HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 //Template engine
-app.engine('.hbs', engine({extname: '.hbs'}));
+app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resource/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.get('/news', (req, res) => {
-  res.render('news');
-});
+// bootstrap
+app.use('/', express.static('./node_modules/bootstrap/dist/'));
 
 app.listen(port, () => {
-  console.log(`Example app listening on at http://localhost:${port}`);
+console.log(`Example app listening on at http://localhost:${port}`);
 });
