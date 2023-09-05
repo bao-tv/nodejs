043d237328f413ab34d5c9e17e1b950d6cf02012
Viet Bao <vietbao.t@gmail.com>
2023-09-05 14:36:22 +0700

    update, edit, delete course

--- src/resource/app/models/Course.js
+++ src/resource/app/models/Course.js
@@ -1,12 +1,22 @@
 const mongoose = require('mongoose');
+const slug = require('mongoose-slug-generator');
+const mongooseDelete = require('mongoose-delete');
 const Schema = mongoose.Schema;
 
 const Course = new Schema({
-  name: {type: String, maxLength: 255},
-  description: {type: String, maxLength: 600},
-  image: {type: String, maxLength: 255},
-  createdAt: {type: Date, default: Date.now},
-  updateAt: {type: Date, default: Date.now},
-});
+  name: {type: String, require: true},
+  description: {type: String},
+  image: {type: String},
+  videoID: {type: String, require: true},
+  slug: {type: String, slug: 'name',unique: true},
+},
+{ timestamps: true }
+);
+
+mongoose.plugin(slug);
+Course.plugin(mongooseDelete , {
+  deletedAt : true,
+  overrideMethods: 'all'
+  });
 
 module.exports = mongoose.model('Course', Course);
